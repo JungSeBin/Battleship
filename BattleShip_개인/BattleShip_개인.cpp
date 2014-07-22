f7529f38b43f019bb28f73188c2cedb12c95b689
sebin <tpqls5144@naver.com>
2014-07-23 02:09:28 +0900

    Battleship

---
+++ BattleShip_개인/BattleShip_개인.cpp
@@ -0,0 +1,18 @@
+// BattleShip_개인.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <time.h>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{	
+	srand(time(NULL));
+
+	long int money = 10000000;
+
+	printf("%d\n", money);
+	return 0;
+}
+
